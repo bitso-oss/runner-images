6dd056d08e856467d6865e858be03e7f4977bd3c
Kevin Darcel <kevin.darcel@bitso.com>
2023-09-21 13:05:05 +0200

    feat: disable pass randomization in ec2-macos-init
    correctly delete bash history

--- images/macos/provision/configuration/finalize-vm-aws.sh
+++ images/macos/provision/configuration/finalize-vm-aws.sh
@@ -3,8 +3,11 @@
 echo "Removing macos-init instance logs..."
 sudo ec2-macos-init clean -all
 
+echo "Disable password randomization in ec2-macos-init..."
+sudo sed -i '' 's/RandomizePassword = true/RandomizePassword = false/g' /usr/local/aws/ec2-macos-init/init.toml
+
 echo "Changing homebrew permissions to runner user..."
 sudo chown -R runner /opt/homebrew
 
 echo "Removing Bash history..."
-rm -f /Users/ec2-user/.bash_history
+history -c
