0410e4183fb08c8a168fdd89a93f7bf0fbc22f84
Kevin Darcel <kevin.darcel@bitso.com>
2023-09-21 10:12:39 +0200

    feat: remove bash history when finalizing

--- images/macos/provision/configuration/finalize-vm-aws.sh
+++ images/macos/provision/configuration/finalize-vm-aws.sh
@@ -5,3 +5,6 @@ sudo ec2-macos-init clean -all
 
 echo "Changing homebrew permissions to runner user..."
 sudo chown -R runner /opt/homebrew
+
+echo "Removing Bash history..."
+rm -f /Users/ec2-user/.bash_history
